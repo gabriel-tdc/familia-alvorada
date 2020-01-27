1b4abf88f87406f47f75ce3ed50e3263595b8634
Gabriel Tedeschi <gabriel-tdc@hotmail.com>
2020-01-26 22:02:18 -0300

    Add files via upload

--- backend/src/models/Dono.js
+++ backend/src/models/Dono.js
@@ -1,1 +1,21 @@
+const mongoose = require('mongoose');
+const PointSchema = require('./utils/PointSchema');
 
+const DonoSchema = new mongoose.Schema({
+    nome: String,
+    logradouro: String,
+    bairro: String,
+    localidade: String,
+    uf: String,
+    complemento: String,
+    cep: String,
+    location:{
+        type: PointSchema,
+        index: '2dsphere'
+    }
+    
+    // foto: String,
+    // cachorros: [String]
+});
+
+module.exports = mongoose.model('Dono', DonoSchema);
