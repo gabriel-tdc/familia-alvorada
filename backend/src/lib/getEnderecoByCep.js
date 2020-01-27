64143bd9aa3c94a8985eb022a570d17c48609380
Gabriel Tedeschi <gabriel-tdc@hotmail.com>
2020-01-26 22:01:58 -0300

    Add files via upload

--- backend/src/lib/getEnderecoByCep.js
+++ backend/src/lib/getEnderecoByCep.js
@@ -1,1 +1,15 @@
+const axios = require('axios');
 
+module.exports = async function getEnderecoByCep(cep){
+    const apiResponse = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+    const {logradouro, bairro, localidade, uf, complemento} = apiResponse.data;
+    const endereco = {
+        logradouro,
+        bairro,
+        localidade,
+        uf,
+        complemento
+    }
+    
+    return endereco;
+}
